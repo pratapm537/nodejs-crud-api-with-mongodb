89df9b53b139c29b95f7a1ec497a99217e11e057
Mohit Pratap Mehra <52151831+pratapm537@users.noreply.github.com>
2026-03-31 20:48:43 +0530

    all routes created

--- index.js
+++ index.js
@@ -8,8 +8,29 @@ dotevn.config({
 const app = express()
 const port = process.env.PORT || 3000
 
+// Routes
 app.get('/', (req,res) => {
     res.send("<h1>Welcome to Home Pages</h1>")
+})
+
+app.get('/show-contact', (req,res) => {
+    res.send("Welcome To Show Contact Page")
+})
+
+app.get('/add-contact', (req,res) => {
+    res.send("Welcome to Add New Contact Page")
+})
+
+app.post('/add-contact',(req,res) => {
+
+})
+
+app.get('/update-contact', (req,res) => {
+    res.send("Welcome to Update Contact Page")
+})
+
+app.post('/update-contact', (res,res) => {
+    
 })
 
 
